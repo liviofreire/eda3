7426b308c2e241377a45d7158fdad674623a8286
Lívio Freire <livio.amf@gmail.com>
2016-08-24 20:24:06 -0300

    first commit

---
+++ src/br/ufc/crateus/eda/Date.java
@@ -0,0 +1,35 @@
+package br.ufc.crateus.eda;
+
+public class Date {
+	private int month;
+	private int day;
+	private int year;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + day;
+		result = prime * result + month;
+		result = prime * result + year;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		if (day != other.day)
+			return false;
+		if (month != other.month)
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
+
+}
